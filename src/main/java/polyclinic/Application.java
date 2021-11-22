d14a37f6fcf6e3aa4bd4b39541c04482391ec60e
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-11-23 00:15:27 +0500

    drop doctors

--- src/main/java/polyclinic/Application.java
+++ src/main/java/polyclinic/Application.java
@@ -1,15 +1,19 @@
 package polyclinic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @ComponentScan("polyclinic.*")
 @EntityScan("polyclinic.*")
 @EnableJpaRepositories("polyclinic.*")
+
 public class Application {
 
 	public static void main(String[] args) {
