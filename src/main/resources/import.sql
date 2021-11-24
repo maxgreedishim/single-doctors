71ab04b5b7ccb5e8338440d35ddeb27da6ffef58
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-11-24 23:33:06 +0500

    move index to files

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
-INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Федор1', 'Федоров', 'Петрович', 'хирург');
+INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Иван', 'Иванов', 'Петрович', 'хирург');
+INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Федор', 'Федоров', 'Петрович', 'доктор');
+INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Петр', 'Петров', 'Петрович', 'терапевт');
 INSERT into patients (first_name, last_name, middle_name, phone_number)   VALUES ('Сидор', 'Сидоров', 'Сидорович', '89270000000');
 INSERT into recipes  (description, doctor, patient, creation_date, validity, priority) VALUES ('Аспирин', 1, 1, '2020-07-11', '2020-11-07', 1);
