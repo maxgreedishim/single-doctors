35e13e680b9e32c68208d42349d3ac49e45c424d
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-11-26 00:21:12 +0500

    сделал поциентов, рецепты не работают

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,4 +2,8 @@ INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES
 INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Федор', 'Федоров', 'Петрович', 'доктор');
 INSERT into doctors  (first_name, last_name, middle_name, specialization) VALUES ('Петр', 'Петров', 'Петрович', 'терапевт');
 INSERT into patients (first_name, last_name, middle_name, phone_number)   VALUES ('Сидор', 'Сидоров', 'Сидорович', '89270000000');
-INSERT into recipes  (description, doctor, patient, creation_date, validity, priority) VALUES ('Аспирин', 1, 1, '2020-07-11', '2020-11-07', 1);
+INSERT into patients (first_name, last_name, middle_name, phone_number)   VALUES ('Максим', 'Максимов', 'Максимович', '89270043530');
+INSERT into patients (first_name, last_name, middle_name, phone_number)   VALUES ('Илья', 'Денисов', 'Денисович', '89272453200');
+INSERT into recipes  (description, doctor, patient, creation_date, validity, priority) VALUES ('Аспирин', 1, 2, '2020-01-02', '2020-11-07', 1);
+INSERT into recipes  (description, doctor, patient, creation_date, validity, priority) VALUES ('Валидол', 2, 3, '2021-02-03', '2021-12-07', 2);
+INSERT into recipes  (description, doctor, patient, creation_date, validity, priority) VALUES ('Карвалол', 3, 2, '2019-03-04', '2019-11-07', 3);
